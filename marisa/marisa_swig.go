652765b35ede64d51e24f4c97fa3d9012f1904c9
Patrick Gaskin <patrick@pgaskin.net>
2020-03-06 00:15:16 -0500

    marisa: Removed pre-generated SWIG wrapper for now (fixes #2)
    This caused runtime failures on 32-bit platforms due to pointer
    size mismatches.

--- marisa/marisa_swig.go
+++ marisa/marisa_swig.go
@@ -3,7 +3,6 @@
 package marisa
 
 //go:generate go run -tags libmarisa_generate libmarisa_generate.go
-//go:generate swig -go -cgo -intgosize 64 -module marisa -outdir . -o marisa.cc -c++ marisa_swig.swigcxx_
 
 //#cgo CPPFLAGS:
 //#cgo LDFLAGS:
